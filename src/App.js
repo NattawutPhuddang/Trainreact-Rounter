ab8928ffcf89b2ebeca3d15dbbdb3286b75ed424
TANATAD1 <MrGladiator012@gmail.com>
2025-07-29 14:42:33 +0700

    update todolist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Counter from './componants/count.jsx';
+import Todolist from './componants/todolist.jsx';
+
 function App() {
   return (
     <div>
+      <div>
       <Counter />
     </div>
+    <div>
+      <Todolist />
+    </div>
+    </div>
   );
-
+  
 }
 
 export default App;
