c9ec1dab42cb4f690c18fb79c7110c80848e41f9
NattawutPhuddang <dreamd397@gmail.com>
2025-07-29 13:36:15 +0700

    Refactor App component to use Counter and remove unused logo

---
+++ src/componants/count.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+      <button onClick={() => setCount(count - 1)}>Subtract</button>
+    </div>
+  );
+}
+
+export default Counter;
