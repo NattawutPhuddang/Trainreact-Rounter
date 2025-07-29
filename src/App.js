3c5780bbab2672dd16992b44351528f8949fe8f0
NattawutPhuddang <dreamd397@gmail.com>
2025-07-29 13:48:02 +0700

    Remove unused elements and clean up App component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import Counter from './componants/count.jsx';
 function App() {
   return (
-counter-number
     <div>
       <Counter />
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React-router ;lk;la;samfsd,d;fdlsldl,fdlfdldsldslf;d;ldlgoo
-        </a>
-      </header>
-main
     </div>
   );
+
 }
 
 export default App;
